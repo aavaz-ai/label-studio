6ca562fbdd2a6574f0440586c3e4fb602febd07a
Aryan Gupta <aryangupta2507@gmail.com>
2022-05-22 23:28:01 +0530

    Test-11

--- label_studio/tasks/api.py
+++ label_studio/tasks/api.py
@@ -358,7 +358,6 @@ class AnnotationsListAPI(generics.ListCreateAPIView):
 
     def perform_create(self, ser):
         task = get_object_with_check_and_log(self.request, Task, pk=self.kwargs['pk'])
-        print(task.json())
         # annotator has write access only to annotations and it can't be checked it after serializer.save()
         user = self.request.user
 
